a13443cf73ffa0f695020ed1abb36341903157f2
mwrng <transit.524@gmail.com>
2024-08-30 14:28:40 +0900

    code 0-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("こんにちは");
+        System.out.println("菅原拓真です");
+        System.out.println("31歳です");
+        System.out.println("お酒が好きです");
     }
 }

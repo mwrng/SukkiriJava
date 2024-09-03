f3f81bd68cb476504138b3958785cf489c1526a9
mwrng <transit.524@gmail.com>
2024-09-04 00:29:51 +0900

    code 1-2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("菅原拓真です");
-        System.out.println("31歳です");
-        System.out.println("お酒が好きです");
-        System.out.println("31 + 31の計算をします");
-        System.out.println(31 + 31);
-        int x;
-        x = 6;
-        System.out.println(x * x * 3.14);
+        int age;
+        age = 30;
+        System.out.println(age);
     }
 }

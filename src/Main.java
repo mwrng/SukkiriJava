6fbd9428cc4e459f826090fa70a563f23c991d7d
mwrng <transit.524@gmail.com>
2024-09-04 00:43:36 +0900

    code 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int age;
+        age = 20;
+        System.out.println("私の年齢は" + age);
         age = 30;
-        System.out.println(age);
+        System.out.println("…いや、本当の年齢は" + age);
     }
 }

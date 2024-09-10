c5b60c0de87da51dc0dd441883bd078400bc3081
mwrng <transit.524@gmail.com>
2024-09-10 13:11:15 +0900

    code 2-11

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
     String name = "すがわら";
-    String message;
-    message = name + "さん、こんにちは";
-    System.out.println(message);
+    System.out.println(name + "さん、こんにちは");
     }
 }

dc719cc145ab9bbb1bf7301a91d52c8f922af7d3
mwrng <transit.524@gmail.com>
2024-09-10 12:49:43 +0900

    colimn ++ --

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String prof1 = "名前：朝香あゆみ\n    （あさかあゆみ）";
-        String prof2 = """
-                       名前：湊雄輔
-                           （みなとゆうすけ）
-                       """;
-        System.out.println(prof1);
-        System.out.println(prof2);
+        int a = 10;
+        int b = 10;
+        System.out.println(++a + 50);
+        System.out.println(b++ + 50);
     }
 }

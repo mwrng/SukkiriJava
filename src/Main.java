8058b6d9804a3a73493ba10adaa84f936ab990d4
mwrng <transit.524@gmail.com>
2024-09-10 12:40:02 +0900

    code 2-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-    System.out.println("私の好きな記号は二重引用符(\")です");
+        String prof1 = "名前：朝香あゆみ\n    （あさかあゆみ）";
+        String prof2 = """
+                       名前：湊雄輔
+                           （みなとゆうすけ）
+                       """;
+        System.out.println(prof1);
+        System.out.println(prof2);
     }
 }
